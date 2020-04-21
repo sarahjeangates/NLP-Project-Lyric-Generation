318277048412eace116ea41601ed258db2ec282c
Sarah Gates <sjg275@gmail.com>
2020-04-21 18:35:03 -0400

    Filter scrape
    Added a filter to remove songs where the artist is only a feature

--- lyric_scrape.py
+++ lyric_scrape.py
@@ -33,31 +33,35 @@ song_lyrics = []
 
 # visit each title, then stores song, artist, and lyrics
 for title in titles:
-    # visit each title
+  # visit each title
   response_title = requests.get(title.attrib['href'])
-    # separate the content
+  # separate the content
   doc2 = pq(response_title.content)
 
-    # find song name and append to list
+
+  # find song name and append to list
   name_full = doc2('h1')
   song_full = str(name_full.text())
   song = song_full.lower()
   song = song[:-7]
   song = song.replace(" ","-")
   song = song.split("---")
-  name = song[1]
-  song_.append(name)
+  
+  # filter out artists that are not requested artist
+  artist = song[0]
+  if artist == artist_i:
+    name = song[1]
+    song_.append(name)
 
     # append artist to list
-  artist = song[0]
-  song_artist.append(artist)
+    artist = song[0]
+    song_artist.append(artist)
 
     # find the song lyrics
-  verse = doc2('.verse')
-  lyrics = verse.text()
+    verse = doc2('.verse')
+    lyrics = verse.text()
     # append the lyrics to list
-  song_lyrics.append(lyrics)
-
+    song_lyrics.append(lyrics)
 print('All done!')
 
 #VALIDATION
@@ -67,7 +71,7 @@ print('All done!')
 
 #%%
 
-# Step 2: export the lyrics to csv
+# Step 2: combine songs, artist, and lyrics lists into Data Frame
 
 # change each list in step 1 to a numpy array
 song = np.asarray(song_)
@@ -76,6 +80,11 @@ lyrics = np.asarray(song_lyrics)
 
 # create a data frame containing the three numpy arrays
 lyrics_csv = pd.DataFrame(np.hstack((song.reshape(-1, 1), artist.reshape(-1, 1), lyrics.reshape(-1, 1))), columns=['song', 'artist', 'lyrics'])
+
+
+#%%
+
+# Step 3: export the lyrics to csv
 
 # NEED TO CHANGE THIS TO GITHUB REPO
 # define directory to export csv file to
